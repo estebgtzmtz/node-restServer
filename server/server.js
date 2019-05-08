02117613394b5fec0295bfba6db1db45be5f9c4e
Esteban <esteb.gtz.mtz@hotmail.com>
2019-05-07 20:23:48 -0500

    Google Sign In
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ require('./config/config');
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Habilitar la carpeta PUBLIC
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Configuracion global de rutas
 app.use(require('./routes/index'));
